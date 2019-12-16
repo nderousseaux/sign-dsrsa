0c9fd713ecfe5d2de87066266dfa92cb429a837a
VAN-DER-SPURT YOAN <yoan.van-der-spurt@etu.unistra.fr>
2019-12-16 14:59:14 +0100

    Redéfinition de decypher + decypherBloc

--- Programme/utils.c
+++ Programme/utils.c
@@ -1,10 +1,10 @@
-
 #include "utils.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
-
+#include <unistd.h>  
 #include <math.h>
+#include <fcntl.h>
 
 uint32_t toPower(uint32_t a, uint32_t b, uint32_t mod){
 	uint32_t res = a;
@@ -14,38 +14,31 @@ uint32_t toPower(uint32_t a, uint32_t b, uint32_t mod){
 	return res;
 }
 
-//Chiffrement d'un message 
-void encryptionMessage(struct public_Key, const char *fileIn, char *fileOut){
-
-	int fd = open(fileIn, O_RDONLY);
-	char* buf[4];
-	int nb = read(fd, buf, 4);
-
-	while( nb >= 4){
-		encryptionBloc(public_Key, buf, fileOut);
-		int nb = read(fd, buf, 4);
-	}
-	encryptionBloc(public_Key, nb, fileOut);
-}
-
-//Chiffrement d'un bloc du fichier qui est ensuite renvoyé dans le fichier de sortie
-void encryptionBloc(struct public_Key, uint8_t *partMessage, fileOut){
-
-	partMessage = puissanceModulo(partMessage, public_Key.e, public_Key.n);
-	write(fileOut, partMessage, 4);
-}
-
-/* Fonction de déchiffrement d'un bloc de 32 bits
-   Arguments:
-   		pk: pointeur sur la clef privée, contenant les valeurs p, q, et l'inverse modulaire d de e, mod n
-   		crypted_message: bloc de longueur 32 bits à déchiffrer
-*/
-uint32_t decipher(struct private_key* pk, uint32_t crypted_message){
+uint32_t decipherBloc(private_key* pk, char* bloc){
 	uint32_t p=pk->p;
 	uint32_t q=pk->q;
 	uint32_t d=pk->d;
 	uint32_t n=p*q;
-	uint32_t decryped_message=(toPower(crypted_message,d,n));
-	return decryped_message;
+	return toPower(*bloc,d,n);
 }
+
+/* Fonction de déchiffrement d'un fichier
+   Arguments:
+   		pk: pointeur sur la clef privée, contenant les valeurs p, q, et l'inverse modulaire d de e, mod n
+   		crypted_message: fichier à déchiffrer
+*/
+void decipher(private_key* pk, char* fileIn, char* fileOut){
+	int fdIn=open(fileIn,O_RDONLY);
+	int fdOut=open(fileOut,O_WRONLY);
+	char buf[4];
+	int n;
+	while((n=read(fdIn,buf,4))){
+		uint32_t decipheredBloc=decipherBloc(pk,buf);
+		write(fdOut,&decipheredBloc,n);
+	}
+	close(fdOut);
+	close(fdIn);
+}
+
+
 
