62c3ed703a20aae203a9c5bb9dc7e27542b2873a
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2019-12-26 18:39:45 +0100

    Remise à jour de l'architecture (les fonctions relatives au rsa dans le fichier rsa et les fonctions relative au sha256 dans le fichier utils) + Création d'un nouveau fichier : sha256, pour tester la fonction rsa.

--- Programme/main.c
+++ Programme/main.c
@@ -1,19 +1,4 @@
-/*
-#include "utils.h"
-
-int main(int argc, char** argv) {
-
-    if (argc != 2)
-    {
-        fprintf(stderr, "Usage: %s messageAHasher\n", argv[0]);
-        return EXIT_FAILURE;
-    }
-
-    //On lance le sha
-    uint32_t* hashTab = malloc(sizeof(uint32_t)*8);
-    sha256((uint8_t*) argv[1], strlen(argv[1]), hashTab);
-*/
-#include "utils.h"
+#include "rsa.h"
 
 int main(){
 	public_key pub_key;
