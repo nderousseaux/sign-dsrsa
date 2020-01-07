9f5a0540c9e0d03c3e62f8ce67948c19dfbd82ce
MARAK ARNAUD <arnaud.marak@etu.unistra.fr>
2020-01-07 15:46:33 +0100

    Test

--- Programme/main.c
+++ Programme/main.c
@@ -1,18 +1,10 @@
 
 #include "utils.h"
+#include <math.h>
 
-int main(){
-	public_key pub_key;
-	
-	pub_key.n=2773;
-	pub_key.e=17;
-
-	private_key priv_key;
-	
-	priv_key.p=47;
-	priv_key.q=59;
-	priv_key.d=157;
- 	
-	
-	return 0;
+ 
+int main(int argc, char *argv[])
+{
+	printf("%d\n", 1 == 1); 
+    return 0;
 }
--- Programme/utils.c
+++ Programme/utils.c
@@ -34,49 +34,61 @@ uint32_t toPower(uint32_t a, uint32_t b, uint32_t mod){
    		crypted_message: bloc de longueur 32 bits à déchiffrer
 */
 
-void decipher(private_key* pk, char* fileIn, char* fileOut){
-	int fdIn=open(fileIn,O_RDONLY);
-	int fdOut=open(fileOut,O_WRONLY);
-	char buf[4];
-	int n;
-	while((n=read(fdIn,buf,4))){
-		uint32_t decipheredBloc=decipherBloc(pk,buf);
-		write(fdOut,&decipheredBloc,n);
-	}
-	close(fdOut);
-	close(fdIn);
-}
+// void decipher(private_key* pk, char* fileIn, char* fileOut){
+// 	int fdIn=open(fileIn,O_RDONLY);
+// 	int fdOut=open(fileOut,O_WRONLY);
+// 	char buf[4];
+// 	int n;
+// 	while((n=read(fdIn,buf,4))){
+// 		uint32_t decipheredBloc=decipherBloc(pk,buf);
+// 		write(fdOut,&decipheredBloc,n);
+// 	}
+// 	close(fdOut);
+// 	close(fdIn);
+// }
 
-/* Fonction de déchiffrement d'un bloc de 32 bits
+/* Fonction de chiffrement d'un bloc de 32 bits
    Arguments:
    		pk: pointeur sur la clef public, contenant les valeurs n, e
    		partMessage: bloc de longueur 32 bits à déchiffrer
 */
 
-uint32_t encryptionBloc(public_key* pk, uint32_t* partMessage){
+uint32_t cipherBloc(public_key* pk, uint32_t* partMessage){
 
 	uint32_t e = pk->e;
 	uint32_t n = pk->n;
 	return toPower(*partMessage, e, n);
 }
 
-/* Fonction de déchiffrement d'un fichier
+/* Fonction de chiffrement d'un fichier
    Arguments:
    		pk: pointeur sur la clef public, contenant les valeurs n, e
    		*fileIn : fichier d'entrée à chiffré
    		*fileOut : fichier sortie représentant le fichier chiffré
 */
 
-void encryptionMessage(public_key* pk, const char *fileIn, char *fileOut){
+void cipherMessage(public_key* pk, const char *fileIn, char *fileOut){
 
 	int fdIn=open(fileIn,O_RDONLY);
 	int fdOut=open(fileOut,O_WRONLY);
 	uint32_t buf[4];
 	int nb;
 	while( (nb=read(fdIn,buf,4)) ){
-		uint32_t cipheredBloc = encryptionBloc(pk, buf);
+		uint32_t cipheredBloc = cipherBloc(pk, buf);
 		write(fdOut,&cipheredBloc,nb);
 	}
+	close(fdIn);
 	close(fdOut);
-	close(fdOut);
+}
+
+// uint8_t powerOf(uint8_t , uint32_t e){
+// 	uint32_t x =  e;
+// 	for(uint32_t a = 1; )
+
+// }
+
+int isRSASignature(public_key* pk, uint32_t* message, uint32_t* answer,uint32_t signature){
+	uint32_t testMessage = hashage(message, "taille", answer);
+	uint32_t testSignature = toPower(signature,pk->e,pk->n); // signature puissance e modulo n
+	return testMessage == testSinature;
 }
